46ffdb472ddc4a8c59a949f47b742b3aead6ed84
Mateusz Szcześniak <Mateusz Szczeniak>
2016-03-01 19:33:52 +0100

    fsad

--- src/main/java/pl/szcze/userserviceproject/controller/UserController.java
+++ src/main/java/pl/szcze/userserviceproject/controller/UserController.java
@@ -5,15 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
 import pl.szcze.userserviceproject.repository.UserRepository;
 
 @RestController
+
+@RequestMapping("${spring.data.rest.base-path}/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/user")
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<String> list() {
         return new ResponseEntity<>("Siema", HttpStatus.OK);
     }
