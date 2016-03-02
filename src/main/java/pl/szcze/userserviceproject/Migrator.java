61c8389011cf53a62b4bcec8c956ded86ec24319
Mateusz Szcześniak <Mateusz Szczeniak>
2016-03-02 21:40:40 +0100

    mircjacje config

--- src/main/java/pl/szcze/userserviceproject/Migrator.java
+++ src/main/java/pl/szcze/userserviceproject/Migrator.java
@@ -5,8 +5,13 @@ import org.flywaydb.core.Flyway;
 public class Migrator {
 
     public static void main(String[] args) {
+
+        final String url = System.getenv("JDBC_DATABASE_URL");
+        final String user = System.getenv("JDBC_DATABASE_USERNAME");
+        final String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
         Flyway flyway = new Flyway();
-        flyway.setDataSource("url", "user", "password");
+        flyway.setDataSource(url, user, password);
         flyway.migrate();
     }
 
