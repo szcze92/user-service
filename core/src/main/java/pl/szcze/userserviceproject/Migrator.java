ed15d3e50d51c5970b0f01f981c8aece5fd4ec62
Mateusz <Mateusz@Szcze>
2016-03-05 12:51:34 +0100

    Test

--- core/src/main/java/pl/szcze/userserviceproject/Migrator.java
+++ core/src/main/java/pl/szcze/userserviceproject/Migrator.java
@@ -9,7 +9,8 @@ public class Migrator {
         final String url = System.getenv("JDBC_DATABASE_URL");
         final String user = System.getenv("JDBC_DATABASE_USERNAME");
         final String password = System.getenv("JDBC_DATABASE_PASSWORD");
-
+        
+        
         Flyway flyway = new Flyway();
         flyway.setDataSource(url, user, password);
         flyway.migrate();
